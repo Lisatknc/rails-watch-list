a3e24f5a67e5ba7e8cd1ca369db79eb39a1bc90f
Lisa Takenouchi <rhrbanb@gmail.com>
2023-08-11 16:58:24 +0900

    Movie lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,7 +20,15 @@ class ListsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+  private
   def list_params
     params.require(:list).permit(:name, :photo)
   end
+
 end
